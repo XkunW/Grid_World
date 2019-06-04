202c22fbe7dd5a3b50d38c3d20ef0df5ec42cb56
XkunW <marshallxkwang@gmail.com>
2019-06-04 17:41:10 -0400

    Changed grid to 3x5

--- gridworld.py
+++ gridworld.py
@@ -5,19 +5,9 @@ def rand_pair(s, e):
     return np.random.randint(s, e), np.random.randint(s, e)
 
 
-# finds an array in the "depth" dimension of the grid
-"""
-def find_location(state, obj):
-    for i in range(0, 4):
-        for j in range(0, 4):
-            if (state[i, j] == obj).all():
-                return i, j
-"""
-
-
 # Initialize stationary grid, all items are placed deterministically
 def init_grid():
-    state = np.zeros((4, 4, 4))
+    state = np.zeros((3, 5, 4))
     # place player
     state[0, 1] = np.array([0, 0, 0, 1])
     # place wall
@@ -25,16 +15,16 @@ def init_grid():
     # place pit
     state[1, 1] = np.array([0, 1, 0, 0])
     # place goal
-    state[3, 3] = np.array([1, 0, 0, 0])
+    state[2, 3] = np.array([1, 0, 0, 0])
 
     return state
 
 
 # Initialize player in random location, but keep wall, goal and pit stationary
-def inti_grid_player():
-    state = np.zeros((4, 4, 4))
+def init_grid_player():
+    state = np.zeros((3, 5, 4))
     # place player
-    state[rand_pair(0, 4)] = np.array([0, 0, 0, 1])
+    state[np.random.randint(0, 3), np.random.randint(0, 5)] = np.array([0, 0, 0, 1])
     # place wall
     state[2, 2] = np.array([0, 0, 1, 0])
     # place pit
@@ -48,22 +38,22 @@ def inti_grid_player():
     p = find_location(state, 1)  # find pit
     if not a or not w or not g or not p:
         # print('Invalid grid. Rebuilding..')
-        return inti_grid_player()
+        return init_grid_player()
 
     return state
 
 
 # Initialize grid so that goal, pit, wall, player are all randomly placed
 def init_grid_rand():
-    state = np.zeros((4, 4, 4))
+    state = np.zeros((3, 5, 4))
     # place player
-    state[rand_pair(0, 4)] = np.array([0, 0, 0, 1])
+    state[np.random.randint(0, 3), np.random.randint(0, 5)] = np.array([0, 0, 0, 1])
     # place wall
-    state[rand_pair(0, 4)] = np.array([0, 0, 1, 0])
+    state[np.random.randint(0, 3), np.random.randint(0, 5)] = np.array([0, 0, 1, 0])
     # place pit
-    state[rand_pair(0, 4)] = np.array([0, 1, 0, 0])
+    state[np.random.randint(0, 3), np.random.randint(0, 5)] = np.array([0, 1, 0, 0])
     # place goal
-    state[rand_pair(0, 4)] = np.array([1, 0, 0, 0])
+    state[np.random.randint(0, 3), np.random.randint(0, 5)] = np.array([1, 0, 0, 0])
 
     a = find_location(state, 3)  # find grid position of player (agent)
     w = find_location(state, 2)  # find wall
@@ -84,13 +74,13 @@ def make_move(state, action):
     wall = find_location(state, 2)
     goal = find_location(state, 0)
     pit = find_location(state, 1)
-    state = np.zeros((4, 4, 4))
+    state = np.zeros((3, 5, 4))
 
     actions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     # e.g. up => (player row - 1, player column + 0)
     new_loc = (player_loc[0] + actions[action][0], player_loc[1] + actions[action][1])
     if new_loc != wall:
-        if (np.array(new_loc) <= (3, 3)).all() and (np.array(new_loc) >= (0, 0)).all():
+        if (np.array(new_loc) <= (2, 4)).all() and (np.array(new_loc) >= (0, 0)).all():
             state[new_loc][3] = 1
 
     new_player_loc = find_location(state, 3)
@@ -107,8 +97,8 @@ def make_move(state, action):
 
 
 def find_location(state, level):
-    for i in range(0, 4):
-        for j in range(0, 4):
+    for i in range(0, 3):
+        for j in range(0, 5):
             if state[i, j][level] == 1:
                 return i, j
 
@@ -126,13 +116,13 @@ def get_reward(state):
 
 
 def display_grid(state):
-    grid = np.zeros((4, 4), dtype=str)
+    grid = np.zeros((3, 5), dtype=str)
     player_loc = find_location(state, 3)
     wall = find_location(state, 2)
     goal = find_location(state, 0)
     pit = find_location(state, 1)
-    for i in range(0, 4):
-        for j in range(0, 4):
+    for i in range(0, 3):
+        for j in range(0, 5):
             grid[i, j] = ' '
 
     if player_loc:
@@ -151,8 +141,3 @@ def display_grid(state):
             grid[pit] = 'L'  # Lose!
 
     return grid
-
-
-# if __name__ == '__main__':
-#     state = init_grid_rand()
-#     display_grid(state)
